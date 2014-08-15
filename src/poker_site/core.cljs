7c2ec1b3d5d34adabeab7e005446481ea6edb6b4
nuwanda <plopes@ua.pt>
2014-08-15 16:48:26 +0100

    fb working with fb-sdk-cljs

--- src/poker_site/core.cljs
+++ src/poker_site/core.cljs
@@ -1,13 +1,17 @@
 (ns poker-site.core
-  (:require-macros)
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [om-tools.core :refer-macros [defcomponent]]))
+            [om-tools.core :refer-macros [defcomponent]]
+            [poker-site.util :as util]
+            [poker-site.facebook :as fb]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (enable-console-print!)
 
-(def app-state (atom {:hand-count 0}))
+(def app-state (atom {:hand-count 0 :logged false}))
 
+;;Component that handles hand count
 (defcomponent hand-counter [data owner]
   (display-name [_]
                   "hand-counter")
@@ -15,7 +19,7 @@
              (let [inc-button (om/get-node owner "inc")]
                (.focus inc-button)))
   (render [_]
-            (dom/div #js {:className "text-center"}
+            (dom/div #js {:className "text-center" :style (util/display (:logged data))}
                      (dom/h3 nil (str "Hand count: "(:hand-count data)))
                      (dom/div #js {:className "btn-group"}
                               (dom/button #js {:ref "inc"
@@ -28,7 +32,28 @@
                                                :style #js {:width "100px"}}
                                           "Reset")))))
 
-(om/root
-  hand-counter
-  app-state
-  {:target (. js/document (getElementById "app"))})
+;;Component that handles fb login
+(defcomponent fb-login [data owner]
+  (display-name [_]
+                "fb-login")
+  (init-state [_]
+              {:fb-events (chan)})
+  (will-mount [_]
+              (let [c (om/get-state owner :fb-events)]
+                (fb/load-sdk (fn []
+                               (fb/init {:appId "304922749669792"
+                                         :status true
+                                         :xfbml true
+                                         :version "v2.0"})
+                               (put! c :loaded)))))
+  (did-mount [_]
+             (let [c (om/get-state owner :fb-events)]
+               (go (loop []
+                     (<! c)
+                     (fb/get-login-status #(om/transact! data :logged (fn [] true)))))))
+  (render [_]
+          (dom/div #js {:id "fb-root"} nil)))
+
+(om/root hand-counter app-state {:target (. js/document (getElementById "app"))})
+
+(om/root fb-login app-state {:target (. js/document (getElementById "fb-app"))})
