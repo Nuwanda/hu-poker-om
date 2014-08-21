c2e81951dc581e157e804d89f3f2fc66a34ec49d
nuwanda <plopes@ua.pt>
2014-08-21 18:39:54 +0100

    branch done

--- src/poker_site/core.cljs
+++ src/poker_site/core.cljs
@@ -2,35 +2,27 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
-            [poker-site.util :as util]
-            [poker-site.facebook :as fb]))
+            [poker-site.facebook :as fb]
+            [poker-site.hand-result :refer [hand-result]]
+            [poker-site.hand-counter :refer [hand-counter]]
+            [poker-site.stats :refer [stats]]))
+
 
 (enable-console-print!)
 
-(def app-state (atom {:hand-count 0 :logged false}))
+(def app-state (atom {:logged false
+                      :hu-state {
+                        :hand-count 35
+                        :player1 {:id 1 :stack 100 :bis 1 :name "Pedro Maia"}
+                        :player2 {:id 2 :stack 200 :bis 2 :name "Pedro Lopes"}}}))
 
-;;Component that handles hand count
-(defcomponent hand-counter [data owner opts]
-  (display-name [_]
-                  "hand-counter")
-  (did-mount [_]
-             (let [inc-button (om/get-node owner "inc")]
-               (.focus inc-button)))
+(defcomponent teste [data owner]
   (render [_]
-            (dom/div {:class "text-center" :style (util/display (:logged data))}
-                     (dom/h3 (str "Hand count: "(:hand-count data)))
-                     (dom/div {:class "btn-group"}
-                              (dom/button {:ref "inc"
-                                           :on-click #(om/transact! data :hand-count inc)
-                                           :class "btn btn-info"
-                                           :style {:width "100px"}}
-                                          "Increment")
-                              (dom/button {:on-click #(om/transact! data :hand-count (fn [_] 0))
-                                           :class "btn btn-danger"
-                                           :style {:width "100px"}}
-                                          "Reset")))))
+          (dom/div
+           (om/build stats data)
+           (om/build hand-result data))))
 
-(om/root hand-counter app-state {:target (. js/document (getElementById "app"))})
+(om/root teste app-state {:target (. js/document (getElementById "app"))})
 
 (om/root fb/login-button
          app-state
