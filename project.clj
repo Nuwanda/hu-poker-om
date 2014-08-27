987171dc8047ca2d9f23c1ddd7afaf7fa43bcee1
nuwanda <plopes@ua.pt>
2014-08-27 20:56:18 +0100

    reverted change

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.7.0"]
                  [prismatic/om-tools "0.3.0" :exclusions [org.clojure/clojure]]
